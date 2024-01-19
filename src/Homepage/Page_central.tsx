067e00eaa94b6a40cbdff442eec1fc49c8cff8dc
Flavio-LP <flavio5070@outlook.com>
2024-01-18 21:32:54 -0300

    Create flipper at front and back in pokemon

--- src/Homepage/Page_central.tsx
+++ src/Homepage/Page_central.tsx
@@ -1,17 +1,32 @@
 import React from "react";
 import './css/pagecentral.css'
 
-function Page_central(){
+function Page_central() {
 
-    return(
-        
+    return (
+
         <div id='pagecentral'>
 
-         <h1 className="name_pokemon">Dragonite</h1>
+            <h1 className="name_pokemon">Dragonite</h1>
 
-         <div id='div_pokemon'>
-            <img className='image_pokemon' src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/149.png'></img>
-         </div>
+            <div id='div_pokemon'>
+
+                <div className='Flipper-cards'>
+                    <div className="Flipper">
+                        <div className="Front">
+                            <img className='image_pokemon' src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/149.png'></img>
+                        </div>
+                        <div className="Back">
+                            <img className='image_pokemon' src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/149.png'></img>
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+               
+            </div>
         </div>
     )
 
